b420ddfbdf0b10e6cb5556d618748a3fef19aaa7
xxsdxmd <xxsdxmd@163.com>
2024-06-29 01:38:12 +0800

    feat: 1. init project

--- op-study-domain/src/main/java/domain/repository/Repository.java
+++ op-study-domain/src/main/java/domain/repository/Repository.java
@@ -15,31 +15,31 @@ public interface Repository<T extends Aggregate<ID>, ID extends Identifier> {
      * 将一个Aggregate附属到一个Repository，让它变为可追踪
      * @param agg
      */
-    void attach(@NotNull T agg);
+    void attach(T agg);
 
     /**
      * 解除一个Aggregate的追踪
      * @param agg
      */
-    void detach(@NonNull T agg);
+    void detach(T agg);
 
     /**
      * id find Agg
      * @param id
      * @return
      */
-    T find(@NonNull ID id);
+    T find(ID id);
 
     /**
      * 将一个Aggregate从Repository移除
      * @param agg
      */
-    void remove(@NonNull T agg);
+    void remove(T agg);
 
     /**
      * 保存一个Agg
      * @param agg
      */
-    void save(@NonNull T agg);
+    void save(T agg);
 
 }
--- op-study-domain/src/main/java/domain/repository/user/UserRepo.java
+++ op-study-domain/src/main/java/domain/repository/user/UserRepo.java
@@ -9,30 +9,30 @@ import lombok.NonNull;
  * @author xxs
  * @Date 2024/6/29 1:05
  */
-public class UserRepo extends Repository<UserEntity, UserId> {
+public class UserRepo implements Repository<UserEntity, UserId> {
 
     @Override
-    public void attach(@NonNull UserEntity agg) {
+    public void attach(UserEntity agg) {
 
     }
 
     @Override
-    public void detach(@NonNull UserEntity agg) {
+    public void detach(UserEntity agg) {
 
     }
 
     @Override
-    public UserEntity find(@NonNull UserId userId) {
+    public UserEntity find(UserId userId) {
         return null;
     }
 
     @Override
-    public void remove(@NonNull UserEntity agg) {
+    public void remove(UserEntity agg) {
 
     }
 
     @Override
-    public void save(@NonNull UserEntity agg) {
+    public void save(UserEntity agg) {
 
     }
 }
